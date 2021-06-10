575a2b861a7e0d60969ee096a4faf9f440d02b4a
ProgerDav <developer.gyulnazaryan@gmail.com>
2021-06-10 17:10:26 +0400

    Getting API_URL from env

--- chit-chat-frontend/src/services/axios/index.js
+++ chit-chat-frontend/src/services/axios/index.js
@@ -1,3 +1,5 @@
 import axios from "axios";
 
-export default axios.create({ baseURL: "http://localhost:8080/api/v1" });
+const baseURL = process.env.API_URL || "http://localhost:8080/api/v1";
+
+export default axios.create({ baseURL });
